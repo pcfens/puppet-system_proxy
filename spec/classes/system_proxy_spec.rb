9549db066507521cdc8cd6c8666fd03da9438609
Phil Fenstermacher <pcfens@wm.edu>
2014-06-26 14:59:58 -0400

    Change syntax in tests for travis

--- spec/classes/system_proxy_spec.rb
+++ spec/classes/system_proxy_spec.rb
@@ -17,12 +17,9 @@ describe 'system_proxy', :type=> :class do
 
       it { should compile }
       it { should contain_class('system_proxy::params') }
-      it { should contain_system_proxy__env_var('HTTP_PROXY')
-        .with_value('http://proxy.example.com:80')}
-      it { should contain_system_proxy__env_var('http_proxy')
-        .with_value('http://proxy.example.com:80')}
-      it { should contain_system_proxy__env_var('PIP_PROXY')
-        .with_value('http://proxy.example.com:80')}
+      it { should contain_system_proxy__env_var('HTTP_PROXY').with_value('http://proxy.example.com:80')}
+      it { should contain_system_proxy__env_var('http_proxy').with_value('http://proxy.example.com:80')}
+      it { should contain_system_proxy__env_var('PIP_PROXY').with_value('http://proxy.example.com:80')}
     end
 
     context 'that doesn\'t need a proxy installed' do
@@ -50,14 +47,10 @@ describe 'system_proxy', :type=> :class do
 
       it { should compile }
       it { should contain_class('system_proxy::params') }
-      it { should contain_system_proxy__env_var('HTTP_PROXY')
-        .with_value('http://proxy.example.com:80')}
-      it { should contain_system_proxy__env_var('http_proxy')
-        .with_value('http://proxy.example.com:80')}
-      it { should contain_system_proxy__env_var('PIP_PROXY')
-        .with_value('http://proxy.example.com:80')}
-      it { should contain_class('system_proxy::redhat')
-        .with_proxy_uri('http://proxy.example.com:80')}
+      it { should contain_system_proxy__env_var('HTTP_PROXY').with_value('http://proxy.example.com:80')}
+      it { should contain_system_proxy__env_var('http_proxy').with_value('http://proxy.example.com:80')}
+      it { should contain_system_proxy__env_var('PIP_PROXY').with_value('http://proxy.example.com:80')}
+      it { should contain_class('system_proxy::redhat').with_proxy_uri('http://proxy.example.com:80')}
     end
 
     context 'that doesn\'t need a proxy installed' do
