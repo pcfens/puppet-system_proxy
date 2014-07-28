037ca1f7b513aef4f38e2565373e4bff8439bd6e
Phil Fenstermacher <pcfens@wm.edu>
2014-07-28 12:08:00 -0400

    Change logic handling a missing IP address

--- lib/puppet/parser/functions/has_ip_in_cidr_list.rb
+++ lib/puppet/parser/functions/has_ip_in_cidr_list.rb
@@ -20,7 +20,7 @@ Returns true if the client has an IP address within any of the given ranges.
       network = IPAddr.new(cidr)
       interfaces.each do |iface|
         address = lookupvar("ipaddress_#{iface}")
-        unless address.nil?
+        unless address == ''
           ip = IPAddr.new(address)
           if network.include?(ip)
             return true
